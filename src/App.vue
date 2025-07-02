bce80df84c11bbdfe2ea40d184d000253c8d81ef
Cornation <2669503659@qq.com>
2025-07-02 17:23:48 +0800

    web缺少缺货清单

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,11 @@ export default {
 </script>
 <style>
 
+/*  */
 body .el-table th.gutter{
       display: table-cell!important;
   }
-  
+
   body .el-table colgroup.gutter{
       display: table-cell!important;
   }
